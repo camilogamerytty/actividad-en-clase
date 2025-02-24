1085796bd673d6314fcd9ec32bcb1dc3f3db3537
RR-SATGO <realsantiago2405@gmail.com>
2025-02-24 13:09:41 -0500

    nuevo archivo

---
+++ suma_2.0.py
@@ -0,0 +1,7 @@
+n=int(input("digite cantidad de numeros a sumar"))
+suma = 0
+for (i=1;i<=n;i++):
+    num_1 = int(input("ingrese el primer numero :"))
+    suma = suma+num_1
+
+print("la suma es: " , suma)
